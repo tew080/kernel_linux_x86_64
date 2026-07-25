zen-kernel/zen-kernel

sched/alt: Drop the task_access_lock glue from syscalls.c
set_user_nice() and __sched_setscheduler() carried CONFIG_SCHED_ALT
blocks only to route the shared syscall code through Project-C's
task_access_lock() instead of task_rq_lock(). Now that the alternative
scheduler provides the mainline task_rq_lock() family with lockdep
pinning, the upstream branch works as-is: take the ifdef blocks out
and let both schedulers run the same locking sequence.

Give update_rq_clock() external linkage so the shared
__sched_setscheduler() can call it, keeping the __always_inline
annotation so call sites inside the scheduler keep the code generation
introduced by commit 37036bde64c9a ("sched/alt: Mark hot scheduler
helpers __always_inline to match mainline"). Remove the now-unused
task_access_lock() wrapper family. Sleeping tasks now take the rq lock
in these paths exactly as mainline, matching the affinity-path
conversion. Also restore the mainline task_rq_lock() wording in the
__set_task_cpu() comment.

Signed-off-by: Steven Barrett <steven@liquorix.net>
Co-Authored-By: Claude Fable 5 <noreply@anthropic.com>

kernel/sched/alt_core.c
@@ -463,7 +463,7 @@ static void update_rq_clock_task(struct rq *rq, s64 delta)
 #endif
 }
 
-static __always_inline void update_rq_clock(struct rq *rq)
+__always_inline void update_rq_clock(struct rq *rq)
 {
 	s64 delta = sched_clock_cpu(cpu_of(rq)) - rq->clock;
 
@@ -1454,7 +1454,7 @@ static void block_task(struct rq *rq, struct task_struct *p)
 static inline void __set_task_cpu(struct task_struct *p, unsigned int cpu)
 {
 	/*
-	 * After ->cpu is set up to a new value, task_access_lock(p, ...) can be
+	 * After ->cpu is set up to a new value, task_rq_lock(p, ...) can be
 	 * successfully executed on another CPU. We must ensure that updates of
 	 * per-task data have been completed by this moment.
 	 */

tew080/kernel_linux_x86_64

kernel/sched/alt_core.c
@@ -451,7 +451,7 @@ static void update_rq_clock_task(struct rq *rq, s64 delta)
 #endif
 }
 
-static inline void update_rq_clock(struct rq *rq)
+inline void update_rq_clock(struct rq *rq)
 {
 	s64 delta = sched_clock_cpu(cpu_of(rq)) - rq->clock;
 
@@ -1438,7 +1438,7 @@ static void block_task(struct rq *rq, struct task_struct *p)
 static inline void __set_task_cpu(struct task_struct *p, unsigned int cpu)
 {
 	/*
-	 * After ->cpu is set up to a new value, task_access_lock(p, ...) can be
+	 * After ->cpu is set up to a new value, task_rq_lock(p, ...) can be
 	 * successfully executed on another CPU. We must ensure that updates of
 	 * per-task data have been completed by this moment.
 	 */